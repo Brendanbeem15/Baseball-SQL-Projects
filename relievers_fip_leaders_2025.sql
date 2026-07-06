1e5881ceeb078cf87acb08ebe60181119e96b4bc
Brendan Behmer <brendanbehmer@gmail.com>
2026-07-06 16:24:51 -0400

    Add FIP leaders

---
+++ relievers_fip_leaders_2025.sql
@@ -0,0 +1,31 @@
+/*
+Question:
+Which starters had the best Fielding Independent Pitching (FIP) in 2025?
+
+Formula:
+FIP = ((13*HR)+(3*(BB+HBP))-(2*K))/IP + 3.135
+
+Notes:
+- Uses Lahman Pitching table.
+- Converts IPouts to innings pitched.
+- Requires at least 30 IP (90 IPouts) and no more than 3 games started.
+- Combines statistics across multiple teams.
+*/
+
+SELECT p.nameFirst, p.nameLast, pt.yearid,
+ROUND(
+    CAST(
+        (
+            (13 * SUM(pt.HR))
+            + (3 * (SUM(pt.BB) + SUM(pt.HBP)))
+            - (2 * SUM(pt.SO))
+        ) AS REAL
+    ) / (SUM(pt.IPouts) / 3.0)+ 3.135,
+    2
+) AS FIP
+FROM People p JOIN Pitching pt ON p.playerID = pt.playerID 
+WHERE pt.yearid = 2025 
+GROUP BY p.playerID, p.nameFirst, p.nameLast
+HAVING SUM(pt.IPouts) >= 90 AND SUM(pt.IPouts) > 0 AND SUM(pt.GS <=3)
+ORDER BY FIP ASC
+LIMIT 10;
